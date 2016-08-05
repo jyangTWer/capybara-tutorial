741895d75eda9ec756427ca3b9e927feb2107f7a
YANG Jing <jyang@thoughtworks.com>
2016-08-05 13:58:12 +0800

    [JY] - should have sucessful message when create new post sucsessfully and there two places will display the post'name in index page.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -3,19 +3,15 @@ require 'rails_helper'
 RSpec.feature 'post feature', type: :feature do
   scenario 'create new post' do
     visit '/posts'
-    sleep(1)
-    # expect(page).not_to have_content('initialize')
-    # expect(page).to have_content('start loading...')
-    # expect(page).to have_content('finish loading')
     click_link('New Post')
     fill_in('post_title', with: 'my first post')
-    sleep(1)
     fill_in('post_content', with: 'I wanna be the full-stack dev')
-    sleep(1)
     click_button('Create Post')
-    sleep(1)
-    visit '/posts'
-    sleep(1)
     expect(page).to have_content('my first post')
+    click_link 'Back'
+    expect(page).not_to have_content('initialize')
+    expect(page).to have_content('start loading...')
+    expect(page.find('#content')).to have_content('my first post')
+    expect(page.find('.top-10-container')).to have_content('my first post')
   end
 end
